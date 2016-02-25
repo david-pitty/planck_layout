818b7146a5c36eaec6459229b1677807a45d6128
David Pitty <dpitty13@gmail.com>
2016-02-24 20:37:31 -0600

    changed from 1 key to 2 key combination to return from plover to qwerty

--- keyboard_custom_layout.c
+++ keyboard_custom_layout.c
@@ -8,9 +8,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LCTL, FUNC(3), KC_LALT, KC_LGUI, FUNC(1), KC_SPC, KC_SPC, FUNC(2), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
   [1] = { /* RAISE */
-    {KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    KC_MINS, KC_TRNS},
+    {KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    KC_MINS, FUNC(4)},
     {S(KC_GRV), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_MINS), KC_GRV,  KC_LBRC, KC_RBRC, KC_BSLS},
-    {FUNC(5),   KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,     KC_F18,  KC_F19,  KC_F20,  KC_RSFT},
+    {FUNC(5),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_RSFT},
     {KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, FUNC(1), KC_TRNS, KC_TRNS, KC_TRNS,    KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS}
   },
   [2] = { /* LOWER */
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_INSERT, FUNC(3), KC_TRNS, KC_TRNS, KC_TRNS, S(KC_INSERT), KC_TRNS, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R}
   },
   [4] = { /* PLOVER */
-    {KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT, KC_M,  KC_N,  KC_V,  KC_C,  KC_LGUI, KC_LALT, KC_NO, KC_NO},
-    {KC_ENT,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, FUNC(4)},
+    {KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT, KC_M,  KC_N,  KC_V,  KC_C,  KC_LGUI, KC_LALT, KC_NO, KC_TRNS},
+    {KC_ENT,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, FUNC(1)},
     {KC_QUOT, KC_SCLN, KC_L,    KC_K,    KC_J,  KC_H,  KC_G,  KC_F,  KC_D,    KC_S,    KC_A,  KC_TAB},
     {KC_LBRC, KC_P,    KC_O,    KC_I,    KC_U,  KC_Y,  KC_T,  KC_R,  KC_E,    KC_W,    KC_Q,  KC_ESC}
   }
