6e688f673bc8afd02d9fe8fdbe09851284c89356
David Pitty <dpitty13@gmail.com>
2016-03-06 19:54:08 -0600

    made plover default layout and added rshift to activate NKRO

--- keyboard_custom_layout.c
+++ keyboard_custom_layout.c
@@ -1,23 +1,23 @@
 #include "planck.h"
 
-#define _QWERTY 0
-#define _PLOVER 1
+#define _PLOVER 0
+#define _QWERTY 1
 #define _NUMS 2
 #define _SYMB 3
 #define _CUSTOM 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_PLOVER] = {
+    {KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT, KC_M,  KC_N,  KC_V,  KC_C,  KC_LGUI, KC_LALT, KC_NO, KC_BSPC},
+    {KC_ENT,  DF(_QWERTY),   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_PAUSE,   KC_RSFT, KC_LSFT},
+    {KC_QUOT, KC_SCLN, KC_L,    KC_K,    KC_J,  KC_H,  KC_G,  KC_F,  KC_D,    KC_S,    KC_A,  KC_TAB},
+    {KC_LBRC, KC_P,    KC_O,    KC_I,    KC_U,  KC_Y,  KC_T,  KC_R,  KC_E,    KC_W,    KC_Q,  KC_0}
+  },
   [_QWERTY] = {
     {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
     {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
     {KC_LCTL, MO(_CUSTOM), KC_LALT, KC_LGUI, MO(_NUMS), KC_SPC, KC_SPC, MO(_SYMB), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
-  },
-  [_PLOVER] = {
-    {KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT, KC_M,  KC_N,  KC_V,  KC_C,  KC_LGUI, KC_LALT, KC_NO, KC_BSPC},
-    {KC_ENT,  DF(_QWERTY),   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_LSFT},
-    {KC_QUOT, KC_SCLN, KC_L,    KC_K,    KC_J,  KC_H,  KC_G,  KC_F,  KC_D,    KC_S,    KC_A,  KC_TAB},
-    {KC_LBRC, KC_P,    KC_O,    KC_I,    KC_U,  KC_Y,  KC_T,  KC_R,  KC_E,    KC_W,    KC_Q,  KC_0}
   },
   [_NUMS] = {
     {KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    KC_MINS, KC_TRNS},
