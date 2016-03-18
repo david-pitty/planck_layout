0d83e8be5beb396fde24cdcbb98f95ad241f89c7
David Pitty <dpitty13@gmail.com>
2016-03-18 11:37:41 -0600

    added key to return to qwerty from plover

--- keyboard_custom_layout.c
+++ keyboard_custom_layout.c
@@ -8,10 +8,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_PLOVER] = {
-    {KC_Q,    KC_W,    KC_E,    KC_R,    KC_H, KC_H,  KC_H,  KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC},
-    {KC_A,    KC_S,    KC_D,    KC_F,    KC_H, KC_H,  KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-    {KC_LSFT, KC_RSFT  KC_PGUP, KC_PGDN, KC_H, KC_H,  KC_H,  KC_ENT, KC_MUTE, KC_VOLD, KC_VOLU, KC_ENT},
-    {KC_0,    KC_TAB,  KC_LALT, KC_C,    KC_V, KC_N,  KC_N,  KC_M,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_Q,    KC_W,    KC_E,    KC_R,    KC_H, KC_H,  KC_H,  KC_U,        KC_I,    KC_O,    KC_P,    KC_LBRC},
+    {KC_A,    KC_S,    KC_D,    KC_F,    KC_H, KC_H,  KC_H,  KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+    {KC_LSFT, KC_RSFT, KC_PGUP, KC_PGDN, KC_H, KC_H,  KC_H,  DF(_QWERTY), KC_MUTE, KC_VOLD, KC_VOLU, KC_ENT},
+    {KC_0,    KC_TAB,  KC_LALT, KC_C,    KC_V, KC_N,  KC_N,  KC_M,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
   [_QWERTY] = {
     {KC_ESC,  KC_Q,        KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC},
