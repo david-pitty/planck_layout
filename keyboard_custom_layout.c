67266232e67cab2d200ae93a65d7f1d5802f5566
David Pitty <david.pitty.ajun@intel.com>
2016-04-03 15:24:38 -0600

    moved backspace and added control key back

--- keyboard_custom_layout.c
+++ keyboard_custom_layout.c
@@ -10,8 +10,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_PLOVER] = {
     {KC_Q,    KC_W,        KC_E,    KC_R,    KC_H, KC_4, KC_H,  KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC},
     {KC_A,    KC_S,        KC_D,    KC_F,    KC_H, KC_0, KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-    {KC_LSFT, KC_0,        KC_PGUP, KC_PGDN, KC_0, KC_0, KC_0,  KC_0, KC_VOLD, KC_VOLU, KC_0,    KC_RSFT},
-    {KC_BSPC, MO(_CUSTOM), KC_LGUI, KC_C,    KC_V, KC_N, KC_N,  KC_M, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_LSFT, KC_BSPC,     KC_PGUP, KC_PGDN, KC_0, KC_0, KC_0,  KC_0, KC_VOLD, KC_VOLU, KC_0,    KC_RSFT},
+    {KC_LCTL, MO(_CUSTOM), KC_LGUI, KC_C,    KC_V, KC_N, KC_N,  KC_M, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
   [_QWERTY] = {
     {KC_ESC,  KC_Q,        KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC},
