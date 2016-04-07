7179b56b911c6b1beaa6adb5af727ac7913a1f0b
David Pitty <dpitty13@gmail.com>
2016-04-06 21:47:37 -0600

    added number bar on third row and moved lost buttons to custom layer

--- keyboard_custom_layout.c
+++ keyboard_custom_layout.c
@@ -8,10 +8,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_PLOVER] = {
-    {KC_Q,    KC_W,        KC_E,    KC_R,    KC_H, KC_4, KC_H,  KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC},
-    {KC_A,    KC_S,        KC_D,    KC_F,    KC_H, KC_0, KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-    {KC_LSFT, KC_BSPC,     KC_PGUP, KC_PGDN, KC_0, KC_0, KC_0,  KC_0, KC_VOLD, KC_VOLU, KC_0,    KC_RSFT},
-    {KC_LCTL, MO(_CUSTOM), KC_LGUI, KC_C,    KC_V, KC_N, KC_N,  KC_M, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_Q,    KC_W,        KC_E,    KC_R, KC_H, KC_BSPC, KC_H,  KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC},
+    {KC_A,    KC_S,        KC_D,    KC_F, KC_H, KC_LSFT, KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+    {KC_0,    KC_0,        KC_0,    KC_0, KC_0, KC_RSFT, KC_0,  KC_0, KC_0,    KC_0,    KC_0,    KC_0},
+    {KC_LCTL, MO(_CUSTOM), KC_LGUI, KC_C, KC_V, KC_N,    KC_N,  KC_M, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
   [_QWERTY] = {
     {KC_ESC,  KC_Q,        KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC},
@@ -34,8 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CUSTOM] = {
     {KC_PSCR,     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_DELETE)},
     {KC_F1,       KC_F2,       KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
-    {DF(_PLOVER), DF(_QWERTY), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN2},
-    {KC_INSERT,   MO(_CUSTOM), KC_TRNS, KC_TRNS, KC_TRNS, S(KC_INSERT), KC_TRNS, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R}
+    {DF(_PLOVER), DF(_QWERTY), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS},
+    {KC_INSERT,   MO(_CUSTOM), KC_TRNS, KC_TRNS, KC_TRNS, S(KC_INSERT), KC_TRNS, KC_BTN1, KC_HOME, KC_PGDN, KC_PGUP, KC_END}
   }
 };
 
