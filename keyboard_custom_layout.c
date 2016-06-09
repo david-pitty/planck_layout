693c51fefb1f7f31467ea1feb6a186e8c6e0e348
David Pitty <dpitty13@gmail.com>
2016-06-09 14:52:10 -0600

    added dvorak layout

--- keyboard_custom_layout.c
+++ keyboard_custom_layout.c
@@ -2,9 +2,10 @@
 
 #define _PLOVER 0
 #define _QWERTY 1
-#define _NUMS 2
-#define _SYMB 3
-#define _CUSTOM 4
+#define _DVORAK 2
+#define _NUMS 3
+#define _SYMB 4
+#define _CUSTOM 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_PLOVER] = {
@@ -18,6 +19,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_TAB,  KC_A,        KC_S,    KC_D,    KC_F,      KC_G,   KC_H,   KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT},
     {KC_LSFT, KC_Z,        KC_X,    KC_C,    KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
     {KC_LCTL, MO(_CUSTOM), KC_LALT, KC_LGUI, MO(_NUMS), KC_SPC, KC_SPC, MO(_SYMB), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  },
+  [_DVORAK] = {
+    {KC_ESC,  KC_QUOT,     KC_COMM, KC_DOT,  KC_P,      KC_Y,    KC_F,    KC_G,      KC_C,    KC_R,    KC_L,    KC_BSPC},
+    {KC_TAB,  KC_A,        KC_O,    KC_E,    KC_U,      KC_I,    KC_D,    KC_H,      KC_T,    KC_N,    KC_S,    KC_SLSH},
+    {KC_LSFT, KC_SCLN,     KC_Q,    KC_J,    KC_K,      KC_X,    KC_B,    KC_M,      KC_W,    KC_V,    KC_Z,    KC_ENT },
+    {KC_LCTL, MO(_CUSTOM), KC_LALT, KC_LGUI, MO(_NUMS), KC_SPC,  KC_SPC,  MO(_SYMB), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
   [_NUMS] = {
     {KC_1,      KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    KC_MINS, KC_TRNS},
