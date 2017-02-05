2eb05e37547abd416a1841959c88d96798f76dd2
David Pitty <david.pitty.ajun@intel.com>
2017-02-05 16:08:38 -0600

    added requires and corrected vcs error

--- manifests/default.pp
+++ manifests/default.pp
@@ -9,7 +9,7 @@ package { $packages:
 }
 
 $path_to_repo = '/home/vagrant/planck_firmware'
-file { 'make a directory for the firmware':
+file { 'firmware directory':
   name      => $path_to_repo,
   ensure    => directory,
 }
@@ -18,19 +18,31 @@ vcsrepo { 'clone firmware with specific commit':
   path     => $path_to_repo,
   ensure   => present,
   provider => git,
-  source   => 'git://github.com/jackhumbert/qmk_firmware.git',
-  revision => 'bdb6dceaebc358dd4aae593d51d3ba0dd61858a9',
+  source   => 'git://github.com/qmk/qmk_firmware.git',
+  #revision => 'bdb6dceaebc357dd4aae593d51d3ba0dd61858a9',
+  require  => [
+    File['firmware directory'],
+    Package[git],
+  ],
+}
+
+# revision attribute erroring out on vcsrepo
+exec { 'change to commit':
+  command => 'git reset --hard bdb6dce',
+  cwd     => $path_to_repo,
 }
 
 file { 'link my layout':
-  path   => "${path_to_repo}/keyboard/planck/keymaps/dpitty.c",
-  ensure => link,
-  target => '/vagrant/keyboard_custom_layout.c',
+  path    => "${path_to_repo}/keyboard/planck/keymaps/dpitty.c",
+  ensure  => link,
+  target  => '/vagrant/keyboard_custom_layout.c',
+  require => Exec ['change to commit'],
 }
 
 file_line { 'ensure NKRO is enabled':
-  ensure => present,
-  path   => "${path_to_repo}/keyboard/planck/Makefile",
-  line   => 'NKRO_ENABLE = yes',
-  match  => '^#\ NKRO_ENABLE',
+  ensure  => present,
+  path    => "${path_to_repo}/keyboard/planck/Makefile",
+  line    => 'NKRO_ENABLE = yes',
+  match   => '^#\ NKRO_ENABLE',
+  require => Exec ['change to commit'],
 }
