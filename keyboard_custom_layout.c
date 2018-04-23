10ccf7143fb85980a5e131ca499c6faaef71542b
David Pitty <david.pitty.ajun@intel.com>
2018-04-23 08:07:50 -0600

    added Alt key in plover layout and moved shift keys for easier NKRO activation

--- keyboard_custom_layout.c
+++ keyboard_custom_layout.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LCTL, MO(_CUSTOM), KC_LALT, KC_LGUI, MO(_NUMS), KC_SPC,  KC_SPC,  MO(_SYMB), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
   [_PLOVER] = {
-    {KC_Q,    KC_W,        KC_E,    KC_R, KC_H, KC_H, KC_H,  KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC},
-    {KC_A,    KC_S,        KC_D,    KC_F, KC_H, KC_H, KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-    {KC_LSFT, KC_RSFT,     KC_0,    KC_0, KC_0, KC_0, KC_0,  KC_0, KC_0,    KC_0,    KC_BSPC, KC_ENT},
-    {KC_LCTL, MO(_CUSTOM), KC_LGUI, KC_C, KC_V, KC_N, KC_N,  KC_M, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_Q,    KC_W,        KC_E,    KC_R, KC_H, KC_LSFT, KC_H,  KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC},
+    {KC_A,    KC_S,        KC_D,    KC_F, KC_H, KC_RSFT, KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+    {KC_LSFT, KC_LALT,     KC_0,    KC_0, KC_0, KC_0,    KC_0,  KC_0, KC_0,    KC_0,    KC_BSPC, KC_ENT},
+    {KC_LCTL, MO(_CUSTOM), KC_LGUI, KC_C, KC_V, KC_N,    KC_N,  KC_M, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
   [_QWERTY] = {
     {KC_ESC,  KC_Q,        KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC},
