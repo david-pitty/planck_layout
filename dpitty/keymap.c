c0b1de2f350e9e603ca1e613de21cf227f0e2552
David Pitty <dpitty13@gmail.com>
2021-12-31 11:01:39 -0600

    removing easy to hit reset and changed plover layout

--- dpitty/keymap.c
+++ dpitty/keymap.c
@@ -98,20 +98,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Plover layer (http://opensteno.org)
  * ,-----------------------------------------------------------------------------------.
- * |   S  |   T  |   P  |   H  |   *  | Exit |   *  |   F  |   P  |   L  |   T  |   D  |
+ * |   #  |   T  |   P  |   H  |   *  | Exit |   *  |   F  |   P  |   L  |   T  |   D  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   S  |   K  |   W  |   R  |   *  |      |   *  |   R  |   B  |   G  |   S  |   Z  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Tab  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  | Bksp |Enter |
+ * |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |  ALT | GUI  |   A  |   O  |      E      |   U  | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 
 [_PLOVER] = LAYOUT_planck_grid(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    EXT_PLV, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+    KC_GRV,  KC_W,    KC_E,    KC_R,    KC_T,    EXT_PLV, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_TAB,  KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_BSPC, KC_ENT,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TAB, KC_ENT,
     KC_LCTL, KC_LALT, KC_LGUI, KC_C,    KC_V,    KC_N,    KC_N,    KC_M,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Plover| Reset|      |      |      |      |      |      |      |      |      |      |
+ * |Plover|Dvorak|      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Insert|      |      |      |      | Shift Insert|      | Prev | Play | Play | Next |
  * `-----------------------------------------------------------------------------------'
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_CUSTOM] = LAYOUT_planck_grid(
     KC_PSCR,   _______, _______, _______, _______, _______,      _______,      _______, _______, _______, _______, S(KC_DELETE),
     KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,        KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    PLOVER,    RESET,   _______, _______, _______, _______,      _______,      _______, _______, _______, _______, _______,
+    PLOVER,    DVORAK,  _______, _______, _______, _______,      _______,      _______, _______, _______, _______, _______,
     KC_INSERT, _______, _______, _______, _______, S(KC_INSERT), S(KC_INSERT), _______, KC_MPRV, KC_MPLY, KC_MPLY, KC_MNXT
 )
 
